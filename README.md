725f2c8ad60209e2565eb04824c45a1d51156e64
Sas Mór <sas.mia.mor@gmail.com>
2026-05-24 09:41:52 +0200

    at lett nevezve és újrarendezve a projekt

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This is a ROS2 project for the Cognitive Robotics Laboratory course of the Mechatronics Engineering MSc of the Budapest University of Technology and Economics created by József Ferenczi, Mór Sas, Máté Horváth and Barnabás Szabó.
 
 The task is to simulate a robot with autonomous driving with additional capabilities of sign or pedestrian recognition.
+
+cd;
+mkdir -p projekt;
+cd projekt;
+git clone https://github.com/barnus877/ros2-sign-recognition;
+
+cd ~/projekt && colcon build && source install/setup.bash && ros2 launch sign_recognition_bringup world_teleopt.launch.py 
+
+ros2 run sign_recognition_py helloka_py
---
+++ src/sign_recognition_py/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'sign_recognition_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='saserq',
+    maintainer_email='sas.mia.mor@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'helloka_py = sign_recognition_py.hello_py:main',
+        ],
+    },
+)
