579d0fb71a9d2e8502f9cf67f6634186cb90efc0
SaDani88 <78sadani@gmail.com>
2026-05-25 18:16:29 +0200

    vonalkövetés hozzáadva
    megbízhatóbb és gyorsabb vonalkövetés P-szabályozóval

--- src/sign_recognition_py/setup.py
+++ src/sign_recognition_py/setup.py
@@ -26,6 +26,8 @@ setup(
         'console_scripts': [
             'helloka_py = sign_recognition_py.hello_py:main',
             'save_training_images = sign_recognition_py.save_training_images:main',
+            'line_follower = sign_recognition_py.line_follower:main',
+            'line_follower_cnn = sign_recognition_py.line_follower_cnn:main'
         ],
     },
 )
