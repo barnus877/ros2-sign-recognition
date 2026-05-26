bc581774f27da3f5b7644fceab08a0f79e07adcd
József Ferenczi <jozsef.ferenczi02@gmail.com>
2026-05-26 22:09:05 +0200

    Tanítási képek

--- src/sign_recognition_bringup/launch/world_teleopt.launch.py
+++ src/sign_recognition_bringup/launch/world_teleopt.launch.py
@@ -62,7 +62,7 @@ def generate_launch_description():
     )
 
     world_arg = DeclareLaunchArgument(
-        'world', default_value='world_with_signs.sdf',
+        'world', default_value='world_with_signs_v3.sdf',
         description='Name of the Gazebo world file to load'
     )
 
